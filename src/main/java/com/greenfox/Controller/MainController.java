9914011cecad29929f7fc3e537b05127641996fa
georgezsiga <georgezsiga@gmail.com>
2017-05-17 11:05:55 +0200

    Add sout to controller

--- src/main/java/com/greenfox/Controller/MainController.java
+++ src/main/java/com/greenfox/Controller/MainController.java
@@ -1,6 +1,10 @@
 package com.greenfox.Controller;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -9,8 +13,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 
+  @ExceptionHandler(Exception.class)
+  public String handleAllExceptions() {
+    return "Error happend";
+  }
+
   @RequestMapping("/")
   public String home() {
+    System.out.println(LocalDate.now().toString() + " " + LocalTime.now().toString() + " INFO" + " Request " + "/index");
     return "index";
   }
 }
