731080328e99cc27fa536d0acb23c890ce7a4cdd
georgezsiga <georgezsiga@gmail.com>
2017-05-17 11:19:06 +0200

    Add getlogmessage method

--- src/main/java/com/greenfox/Logic.java
+++ src/main/java/com/greenfox/Logic.java
@@ -1,8 +1,15 @@
 package com.greenfox;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 /**
  * Created by georgezsiga on 5/17/17.
  */
 public class Logic {
+
+  public String getLogMessage(String path) {
+    return LocalDate.now().toString() + " " + LocalTime.now().toString() + " INFO" + " Request " + path;
+  }
 
 }
