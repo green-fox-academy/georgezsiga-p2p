198c8c44e1d35936c6c46dced11049c2d4568b44
georgezsiga <georgezsiga@gmail.com>
2017-05-22 12:41:47 +0200

    new commit

--- src/main/java/com/greenfox/Controller/MainController.java
+++ src/main/java/com/greenfox/Controller/MainController.java
@@ -19,6 +19,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+
 /**
  * Created by georgezsiga on 5/17/17.
  */
@@ -46,6 +48,7 @@ public class MainController {
     return "index";
   }
 
+  @Messa
   @RequestMapping("/")
   public String home(@RequestParam(value = "error", required = false) String error, Model message,
       Model model, Model id) {
