a5aef53e0645e31a2cc6509aa4a0ef2a7af9bd98
georgezsiga <georgezsiga@gmail.com>
2017-05-25 11:57:01 +0200

    Delete unused code

--- src/main/java/com/greenfox/Repository/MessageRepository.java
+++ src/main/java/com/greenfox/Repository/MessageRepository.java
@@ -3,14 +3,8 @@ package com.greenfox.Repository;
 import com.greenfox.Model.Message;
 import java.sql.Timestamp;
 import java.util.List;
-import javax.persistence.NamedNativeQuery;
-import javax.persistence.QueryHint;
-import org.hibernate.criterion.Distinct;
-import org.hibernate.sql.Select;
-import org.springframework.data.annotation.QueryAnnotation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 /**
@@ -18,24 +12,16 @@ import org.springframework.data.repository.query.Param;
  */
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
-
   List<Message> findAllByOrderByTimestampDesc();
 
   @Query("SELECT DISTINCT username, timestamp FROM Message order by timestamp DESC")
-  List<Message> findAllByOrderByTimestampAsc();
-
-  List<Message> findAllByTimestampGreaterThan(@Param("timestamp") Timestamp timestamp);
 
   List<Message> findAllByUsername(@Param("username") String username);
 
   @Query("select DISTINCT username from Message order by username ASC")
   List<Message> findAllByOrderByTimestamp();
 
-  Message findMessageById(@Param("id") Long id);
-
   List<Message> findAllByUsernameOrderByTimestampDesc(@Param("username") String username);
 
   List<Message> findAllByTimestampIsAfterOrderByTimestampDesc(@Param("timestamp") Timestamp timestamp);
-
-
 }
